98173e53d33b042fa5dd8f5c03a752fee0e4891e
Tyuiu.VlasenkoAE@outlook.com <artemvlasenkoru@mail.ru>
2025-10-16 23:18:39 +0500

    Task6

---
+++ Tyuiu.VlasenkoAE.Sprint2.Task6.V1/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.VlasenkoAE.Sprint2.Task6.V1.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #2 | Выполнил: Власенко А. Е. | ИСТНб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #2                                                               *");
+Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+Console.WriteLine("* Задание #6                                                              *");
+Console.WriteLine("* Вариант #1                                                              *");
+Console.WriteLine("* Выполнил: Власенко Артём Евгеньевич | ИСТНб-25-1                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая использует сокращенную форму записи         *");
+Console.WriteLine("* оператора switch вычисляет требуемое значение и возвращает результат.   *");
+Console.WriteLine("* По данному номеру месяца, определите количество дней в этом месяце.     *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Введите номер месяца: ");
+int numMouth = Convert.ToInt32(Console.ReadLine());
+
+string res;
+
+if ((numMouth < 1) || (numMouth > 12))
+{
+    res = "Введено неверное значение!";
+}
+else
+{
+    res = "Количество дней в этом месяце: " + ds.FindMonthDaysCount(numMouth);
+}
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine(res);
+
+Console.ReadKey();
