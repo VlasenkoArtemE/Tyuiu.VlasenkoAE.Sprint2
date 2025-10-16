f1ad3a04da62c03260388886d3bfe8a182594102
Tyuiu.VlasenkoAE@outlook.com <artemvlasenkoru@mail.ru>
2025-10-16 22:51:21 +0500

    Task5

---
+++ Tyuiu.VlasenkoAE.Sprint2.Task5.V2/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.VlasenkoAE.Sprint2.Task5.V2.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #2 | Выполнил: Власенко А. Е. | ИСТНб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #2                                                               *");
+Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+Console.WriteLine("* Задание #5                                                              *");
+Console.WriteLine("* Вариант #2                                                              *");
+Console.WriteLine("* Выполнил: Власенко Артём Евгеньевич | ИСТНб-25-1                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая использует оператор switch вычисляет        *");
+Console.WriteLine("* требуемое значение и возвращает результат.                              *");
+Console.WriteLine("* По данному месяцу, определите в какую пору года попадает этот месяц     *");
+Console.WriteLine("* (Зима, Лето, Весна, Осень).                                             *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Введите номер месяца: ");
+int numMouth = Convert.ToInt32(Console.ReadLine());
+
+string res;
+
+if ((numMouth < 1) || (numMouth > 12))
+{
+    res = "Введено неверное значение!";
+}
+else
+{
+    res = "Это пора года: " + ds.FindMonthSeason(numMouth);
+}
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine(res);
+
+Console.ReadKey();
